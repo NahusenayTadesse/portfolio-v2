5805fcb02d10dff0b3540494cd7f87794bcd2985
NahusenayTadesse <124673344+NahusenayTadesse@users.noreply.github.com>
2025-05-29 18:20:48 +0300

    Final

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -13,7 +13,14 @@
     <NavUl>
         {#each sections as section}
             <NavLi href="/#{section}" >
-               <Button>{section}</Button> 
+            <button
+                class="px-4 py-2 rounded-lg font-semibold transition-all duration-200
+                             bg-gradient-to-r from-primary-500 to-primary-700 text-white shadow-md
+                             hover:from-primary-600 hover:to-primary-800 hover:scale-105 hover:shadow-lg
+                             active:scale-95 active:shadow-inner focus:outline-none focus:ring-2 focus:ring-primary-400"
+            >
+                {section}
+            </button>
             </NavLi>
         {/each}
     </NavUl>
--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Heading, P, Button } from "flowbite-svelte";
   import { DownloadOutline } from "flowbite-svelte-icons";
+  import { onMount } from "svelte";
+  import { fly, fade } from "svelte/transition";
 
 	function typewriter(node, { speed = 1 }) {
 		const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE;
@@ -20,27 +22,44 @@
 			}
 		};
 	}
+    let showHeading = false; // A reactive variable to control visibility
+
+  onMount(() => {
+    // Set showHeading to true after the component is mounted
+    // A small setTimeout can help ensure the DOM is ready for the transition
+    setTimeout(() => {
+      showHeading = true;
+    }, 50); // Adjust delay as needed
+  });
 </script>
 
 
-<div class="text-center h-lvh sm:p-4" id="Home">
+<div class="text-center sm:p-4" id="Home">
    
-    <br><br><br><br>
-   
+    <br><br>
+    {#if showHeading}
     <Heading tag="h1" class="mb-4 text-4xl font-extrabold  md:text-5xl lg:text-6xl" >
         <span transition:typewriter>Design.Develop.Deliver</span>
     </Heading>
+    {/if}
     <P class="mb-6 text-lg sm:px-16 lg:text-xl xl:px-48 dark:white-400 text-center"> 
         I am Nahusenay, I develop fullstack websites fast and secure. I take care of the designing, the developing, security, deployment and seo.</P>
-    
+   {#if showHeading} 
     <div class="text-center flex gap-6 align-center justify-center center flex-wrap">
-         <Button href="/#Contact" outline>
+        <span in:fly={{ x: -200, duration: 800, opacity: 0 }}
+        out:fade={{ duration: 500 }}><Button href="/#Contact" outline>
             Contact Me  
-         </Button>
-         <Button href="/cv.pdf" download="Nahusenay Tadesse CV and Portfolio.pdf">
+         </Button></span> 
+
+         <span in:fly={{ x: 200, duration: 800, opacity: 0 }}
+        out:fade={{ duration: 500 }}><Button href="/cv.pdf" download="Nahusenay Tadesse CV and Portfolio.pdf">
 Download CV    
 <DownloadOutline />
-  </Button>
+  </Button></span>
+
+         
 </div>
+    {/if}
+    <br><br>
 </div>
 
